dc24bf20a245c8e2958fd4ba668e109c8ffd6376
Devi2605 <devibosch2605@gmail.com>
2025-10-07 16:43:59 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -13,25 +13,12 @@ char size(int cms) {
     return sizeName;
 }
 
-// void testTshirtSize() {
-//     std::cout << "\nTshirt size test\n";
-//     assert(size(37) == 'S');
-//     assert(size(40) == 'M');
-//     assert(size(43) == 'L');
-//     std::cout << "All is well (maybe!)\n";
-// }
-
 void testTshirtSize() {
     std::cout << "\nTshirt size test\n";
-
-    assert(size(37) == 'S');  // Passes
-    assert(size(40) == 'M');  // Passes
-    assert(size(43) == 'L');  // Passes
-
-    // These will fail because the function returns '\0'
-    assert(size(38) == 'S');  // FAILS: 38 is not < 38 or > 38
-    assert(size(42) == 'M');  // FAILS: 42 is not > 42 or < 42
-
-    std::cout << ""All is well (maybe!)\n");
+    assert(size(37) == 'S');
+    assert(size(40) == 'M');
+    assert(size(43) == 'L');
+    std::cout << "All is well (maybe!)\n";
 }
+
 
